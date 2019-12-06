226dc5263cfd1df58f80e2d7520fd40818bd9e16
samiulsami <33352407+samiulsami@users.noreply.github.com>
2019-12-07 00:20:48 +0600

    Added some comments

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -17,7 +17,6 @@ namespace WindowsFormsApp1
         IList<Course> courses;
         Dictionary<string, int> inDegree,Credits;
         Dictionary<string, List<string>> graph;
-        Dictionary<string, bool> takenCourses;
         int totalCredits = 0;
 
         private void label2_Click(object sender, EventArgs e)
@@ -32,24 +31,26 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
 
+            //Read file containing the pre-requisites for each course in the CSE program
+            //COURSES WITH CREDI REQUIREMENTS ARE NOT INCLUDED YET (e.g; RESEEARCH METHODOLOGY, THESIS, INTERNSHIP, etc.)
             String jsonresult = File.ReadAllText(@"CSE Prerequisites.json");
             courses = JsonConvert.DeserializeObject<List<Course>>(jsonresult);
 
             inDegree = new Dictionary<string, int>();
             graph = new Dictionary<string, List<string>>();
             Credits = new Dictionary<string, int>();
-            takenCourses = new Dictionary<string, bool>();
            
+            //Initialize graph and other variables
             for(int i=0; i<courses.Count; i++)
             {
                 string courseName = courses[i].CourseName;
                 List<string> newlist = new List<string>();
                 graph.Add(courseName, newlist);
-                Credits[courseName] = courses[i].Credits-48;
+                Credits[courseName] = courses[i].Credits-48;//Credits stored as ASCII characters in file
                 inDegree[courseName] = 0;
-                takenCourses[courseName] = false;
             }
 
+            //Create a Directed Acyclic Graph with courses as vertices, and calculate In-Degree of each vertex
             for(int i=0; i<courses.Count; i++)
             {
                 if (courses[i].prerequisites[0] == "NIL") continue;
@@ -65,6 +66,10 @@ namespace WindowsFormsApp1
 
         private void populateDataGrids()
         {
+            // Populates the data-grids with available courses and taken courses
+            // Taken courses have in-degree -1
+            // Available courses have in-degree 0
+
             dataGridView1.Rows.Clear();
             dataGridView2.Rows.Clear();
             totalCredits = 0;
@@ -72,15 +77,16 @@ namespace WindowsFormsApp1
             {
                 if (inDegree[crs.CourseName] == 0)
                     dataGridView1.Rows.Add("+", crs.CourseName);
-                else if (inDegree[crs.CourseName] < 0)
+                else if (inDegree[crs.CourseName] ==-1)
                 {
                     dataGridView2.Rows.Add("-", crs.CourseName);
                     totalCredits += Credits[crs.CourseName];
-                }
-                
+                }                
             }
 
+            //Display total completed credits
             label2.Text = totalCredits.ToString();
+
 
             dataGridView1.Sort(dataGridView1.Columns[1], ListSortDirection.Ascending);
             dataGridView2.Sort(dataGridView2.Columns[1], ListSortDirection.Ascending);
@@ -88,17 +94,20 @@ namespace WindowsFormsApp1
         }
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+
             int i = e.RowIndex;
             int j = e.ColumnIndex;
-            if (j == 0 && i>=0){
-                string courseName = dataGridView1.Rows[i].Cells[1].Value.ToString();
 
-                inDegree[courseName]--;
-                takenCourses[courseName] = true;
-                foreach (string nxt in graph[courseName])
-                {
-                    inDegree[nxt]--;
-                }
+            //When user Adds a course by clicking the "+" button
+            //Note: all courses shown in the left data grid have in-degree 0
+            if (j == 0 && i>=0){   
+                
+                //Decrement In-degree of selected course and set it to -1
+                //Decrement In-degree of all child vertices of selected course
+                string courseName = dataGridView1.Rows[i].Cells[1].Value.ToString();
+                inDegree[courseName]--;                
+                foreach (string nxt in graph[courseName])                
+                    inDegree[nxt]--;                
 
                 populateDataGrids();            
             }
@@ -108,34 +117,35 @@ namespace WindowsFormsApp1
         {
             int i = e.RowIndex;
             int j = e.ColumnIndex;
+
+            //When user removes a taken course by clicking the "-" button
+            //Note: all taken courses have in-degree -1
             if (j == 0 && i >= 0)
             {
+                //Run a BFS from selected vertex and only explore a vertex(course) if it has been taken
+                //Increment in-degree of all child vertices by 1
+                //Increment in-degree of all Taken courses(vertices) by 2 instead, since their in-degree is -1
                 string courseName = dataGridView2.Rows[i].Cells[1].Value.ToString();
 
                 Queue<string> q = new Queue<string>();
                 q.Enqueue(courseName);
                 inDegree[courseName]++;
-                takenCourses[courseName] = false;
+                //takenCourses[courseName] = false;
                 while (q.Any())
                 {
                     string front = q.Dequeue();
                     foreach (string s in graph[front])
                     {
-                        if (takenCourses[s])
+                        if (inDegree[s]==-1)
                         {
                             q.Enqueue(s);
                             inDegree[s] += 2;
-                            takenCourses[s] = false;
                         }
-                        else
-                        {
-                            inDegree[s]++;
-                        }
+                        else inDegree[s]++;                        
                     }
                 }
 
-
-               populateDataGrids();
+                populateDataGrids();
             }
         }
     }
